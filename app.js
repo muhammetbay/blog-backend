73a1bb8ba15530c35616c15232ed0df989f69ab0
muhammetbay <mbay454@gmail.com>
2025-06-27 11:22:44 +0300

    environment variables, github actions

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-// app.js
-require('dotenv').config()
+const env = process.env.NODE_ENV || 'development'
+
+require('dotenv').config({ path: `.env.${env}` })
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
